837a4da611873f2388a48cad11c20e6ae04681c7
Martin <malonso@satellogic.com>
2021-05-10 12:41:46 -0300

    Add setup.py and requirements files

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='Powerbar',
+    version='0.0.1',
+    author='Martin Alonso',
+    author_email='alonso.tincho@gmail.com',
+    description='Utils segments to use with powerline',
+    packages= [
+        'powerbar.segments',
+    ],
+    url='http://github.com/maralonso/powerbar',
+)
